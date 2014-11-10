121ac7b8f17ee2a3b96a503800402b8f88f88b14
Adrià Verdaguer <adriaverdaguer@gmail.com>
2014-11-10 23:44:10 +0100

    Wordpress – Static content on Posts Page
    Update

--- static-content-posts-page.php
+++ static-content-posts-page.php
@@ -15,13 +15,6 @@ if ( 'page' == get_option('show_on_front') && get_option('page_for_posts') && is
 	setup_postdata(get_page($page_for_posts_id));
 ?>
 	<header class="entry-header">
-		<?php 
-		$media = wolf_post_media();
-		if ( $media && ! post_password_required() ) : ?>
-			<div id="post-media-container" class="clearfix<?php echo $custom_bg_class; ?>"<?php echo $custom_bg; ?>>
-				<?php echo $media; ?>
-			</div>
-		<?php endif; ?>
 		<?php the_content(); ?>
 	</header><!-- header.entry-header -->
 <?php
